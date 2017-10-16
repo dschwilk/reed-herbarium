06331a96e3dd62a07f4e6c861fb80ee750e1cae0
Dylan W. Schwilk <dylan@schwilk.org>
2017-10-16 09:49:01 -0500

    Fix data reading to strip white space

--- scripts/read_data.R
+++ scripts/read_data.R
@@ -7,16 +7,18 @@ library(dplyr)
 # read linked tables
 # these data files are tabbed-deliminated with no quoted fields and no tabs allowed
 ttc <- read.csv("../data/ttc.csv", sep="\t", quote="", stringsAsFactors=FALSE,
-                na.strings=c("","NA"))
+                na.strings=c("","NA"), strip.white=TRUE)
 collections <- read.csv("../data/collections.csv", sep="\t", quote="",
-                        stringsAsFactors=FALSE, na.strings=c("","NA"))
+                        stringsAsFactors=FALSE, na.strings=c("","NA"),
+                        strip.white=TRUE)
 managed_area_types <-  read.csv("../data/managed_area_types.csv", sep="\t",
                                 quote="", stringsAsFactors=FALSE,
                                 na.strings=c("","NA"))
 
 # some cleaning
-ttc <- mutate(ttc, location=ifelse(is.na(location), verbatim_location, location),
-              year=as.numeric(year),
-              month=as.numeric(month),
-              day=as.numeric(day))
+## ttc <- mutate(ttc, locality=ifelse(is.na(locality), verbatim_locality, locality),
+##               year=as.numeric(year),
+##               month=as.numeric(month),
+##               day=as.numeric(day))
+
 
