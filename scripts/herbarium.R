871a948e5d6ddc4afe54c9846d36b5858c796f1a
Dylan W. Schwilk <dylan@schwilk.org>
2017-10-24 10:53:16 -0500

    Update data exploration script

--- scripts/herbarium.R
+++ scripts/herbarium.R
@@ -10,35 +10,21 @@ names(ttc)
 nrow(ttc)
 
 # number with status=="v"
-nrow(subset(ttc, processing_status=="v"))
-
-nrow(subset(ttc, !is.na(processing_status)))
-
-length(unique(ttc$AN_TTC))
-
-# lets look just at entries with an AN
-ttc.an <- subset(ttc, !is.na(AN_TTC) & AN_TTC!="000000")
-nrow(ttc.an)
+nrow(subset(ttc, processingStatus=="v"))
+nrow(subset(ttc, !is.na(processingStatus)))
 
 # Number reviewed and confirmed:
-ttc.reviewed <- ttc.an %>% subset(!is.na(date_reviewed))
+ttc.reviewed <- ttc %>% subset(!is.na(dateReviewed))
 nrow(ttc.reviewed)
-
-
-ttc <- filter(ttc, processing_status != "missing")
 
 ## groups
 unique(ttc$category)
-
-filter(ttc, category=="LC")
-filter(ttc, category=="BN")
-
 
 ## Collections by year ##
 byear <- ttc %>% group_by(year) %>% summarize(count=length(year))
 ggplot(aes(as.numeric(year), count), data=byear) + geom_line() +
   xlab("year") + ylab("# specimens collected")
-ggsave("../results/collections_by_year.pdf")
+ggsave("../results/collections_by_year.png")
 
 ## Collections by plant family ##
 byfamily <- ttc %>% group_by(family) %>% summarize(count=length(year)) %>%
@@ -46,15 +32,21 @@ byfamily <- ttc %>% group_by(family) %>% summarize(count=length(year)) %>%
 
 ggplot(aes(family, count), data=byfamily[1:10,]) + geom_bar(stat="identity") +
   xlab("family") + ylab("# specimens collected")
-ggsave("../results/collections_by_family.pdf")
+ggsave("../results/collections_by_family.png")
 
 ## by state/province
-bystate <- ttc %>% group_by(state_province) %>% summarize(count=length(year)) %>%
+bystate <- ttc %>% group_by(stateProvince) %>% summarize(count=length(year)) %>%
   slice(order(count, decreasing=TRUE))
 
-ggplot(aes(state_province, count), data=bystate[1:10,]) + geom_bar(stat="identity") +
+ggplot(aes(stateProvince, count), data=bystate[1:10,]) + geom_bar(stat="identity") +
   xlab("state/province") + ylab("# specimens collected")
-ggsave("../results/collections_by_state.pdf")
+ggsave("../results/collections_by_state.png")
+
+ggplot(bystate[1:10,] ,aes(stateProvince, count)) + geom_bar(stat="identity") +
+  xlab("state/province") + ylab("# specimens collected")
+
+
+
 
 ## by country
 bycountry <- ttc %>% group_by(country) %>% summarize(count=length(year)) %>%
@@ -63,22 +55,7 @@ bycountry <- ttc %>% group_by(country) %>% summarize(count=length(year)) %>%
 ggplot(aes(country, count), data=bycountry) + geom_bar(stat="identity") +
     xlab("country") + ylab("# specimens collected") +
     scale_y_log10()
-ggsave("../results/collections_by_country.pdf")
+ggsave("../results/collections_by_country.png")
 
-
-# lets look just at entries with an AN
-ttc.an <- subset(ttc, !is.na(AN_TTC) & AN_TTC!="000000")
-nrow(ttc.an)
-     
-state_mex <- subset(ttc, state_province=="Mexico") %>% select(AN_TTC, family, genus, species, country, state_province)
-
-subset(ttc, country=="USA" & state_province=="Mexico")
-
-ttc.reviewed <- ttc.an %>% filter(!is.na(date_reviewed))
-# subset(ttc.reviewed, country=="USA" & state_province=="Mexico")
-
-
-ggplot(aes(state_province, count), data=bystate[1:10,]) + geom_bar(stat="identity") +
-  xlab("state/province") + ylab("# specimens collected")
 
 
